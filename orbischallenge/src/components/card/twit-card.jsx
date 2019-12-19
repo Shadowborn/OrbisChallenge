d78947dcdceced256a0e8490bfe1efe2b4316a44
Shadowborn <james.starks023@gmail.com>
2019-12-19 15:30:53 -0700

    usernames added

--- orbischallenge/src/components/card/twit-card.jsx
+++ orbischallenge/src/components/card/twit-card.jsx
@@ -4,6 +4,7 @@ import './card.styles.css'
 export const Twit = (props) => (
     <div className="card-container"> 
         <img alt="monster" src={props.twit.user.avatar_url}/>
+        <h1 className="card-username">{ props.twit.user.username }</h1>
         <p>{ props.twit.body }</p>
         {console.log("Props", props)}
     </div>

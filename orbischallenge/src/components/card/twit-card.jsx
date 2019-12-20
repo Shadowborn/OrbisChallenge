ad9c6e088ad058d8e3461d44a725ecf650d2b11d
Shadowborn <james.starks023@gmail.com>
2019-12-19 18:31:08 -0700

    symbols added

--- orbischallenge/src/components/card/twit-card.jsx
+++ orbischallenge/src/components/card/twit-card.jsx
@@ -3,9 +3,14 @@ import './card.styles.css'
 
 export const Twit = (props) => (
     <div className="card-container"> 
+    
         <img alt="monster" src={props.twit.user.avatar_url}/>
+    <div className="card-header">
         <h1 className="card-username">{ props.twit.user.username }</h1>
+        <h1 className="card-username">Symbol: { props.twit.symbols[0].symbol }</h1>
+    </div>
         <p>{ props.twit.body }</p>
+    
         {console.log("Props", props)}
     </div>
 );

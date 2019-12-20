5d38485c9e75f754dfcb3cb2c4d4a4a404c599ca
Shadowborn <james.starks023@gmail.com>
2019-12-19 18:07:37 -0700

    getting ready for netlify

--- orbischallenge/src/App.js
+++ orbischallenge/src/App.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 
-import { CardList } from './components/card-list/card-list.component'
 import { TwitList } from './components/card-list/twits-list.jsx'
 import { SearchBox } from './components/search-box/search-box.component'
 
@@ -9,26 +8,25 @@ import axios from 'axios'
 
 class App extends Component {
   constructor() {
-    super(); //Super calls the constructor method, on the component class and gives us access to this.state
+    super(); 
 
     this.state = {
       monsters: [],
       twits: [],
       searchField: ''
     }
-    
-    // this.handleChange = this.handleChange.bind(this)
-    // ^^ this would only need to be here if my handleChange method is not an arrow function, since arrow functions automatically bind this to the class component. (Lexical scoping, binding "this" to where it was defined in the first place)
+  
 
   }
 
 
   async componentDidMount() {
-    const monsters = await axios.get('https://jsonplaceholder.typicode.com/posts')
     
-    const twits = await axios.get('https://orbis-twits.herokuapp.com/')
+    const twits = await axios.get('https://orbis-twits.herokuapp.com')
 
-    this.setState({...this.state, monsters, twits: twits.data.messages})
+    // const twits = await axios.get('https://api.stocktwits.com/api/2/streams/trending.json')
+
+    this.setState({...this.state, twits: twits.data.messages})
     
   }
 
@@ -39,21 +37,13 @@ class App extends Component {
 
 
   render() {
-    const { monsters, twits, searchField } = this.state; //destructuring, pulling properties off of this.state, and setting them to constants called monsters and searchField
+    const { twits, searchField } = this.state; 
 
-    // const twitsArray = Object.values(twits).map(monster => {
-    //   return monster[2];
-    // });
-
-    // const filteredMonsters = monsters.filter(monster => 
-    //     monster.title.toLowerCase().includes(searchField.toLowerCase())
-    //   )
       
     const filteredTwits = twits.filter(twit => 
         twit.body.toLowerCase().includes(searchField.toLowerCase())
       )
 
-      console.log("Monsters",monsters)
       console.log("Twits", twits)
     return (
       <div className='App'>
@@ -62,7 +52,6 @@ class App extends Component {
           placeholder={ 'search..' }
           handleChange={ this.handleChange }
         />
-        {/* <CardList monsters={filteredMonsters}/> */}
         <TwitList twits={filteredTwits}/>
         
       </div>
--- orbischallenge/src/components/search-box/search-box.component.jsx
+++ orbischallenge/src/components/search-box/search-box.component.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 import './search-box.styles.css';
 
-//functional components don't have access to state or lifescycle methods, functional components just get some props and return something
 export const SearchBox = ({ placeholder, handleChange }) => (
     
     <input
